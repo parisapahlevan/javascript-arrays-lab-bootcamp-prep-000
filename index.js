8268dfb8ea1fed96f070a5d39f92dfbeabf5011b
parisa pahlevan <paris.pahlevan@gmail.com>
2018-08-28 13:52:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,52 +1,36 @@
-var kittens = ['Milo', 'Otis', 'Garfield'];
-function destructivelyAppendKitten(name){
-  return kittens.push('Ralph')
- }
- var result = destructivelyAppendKitten(kittens)
- console.log(result);
- 
- function destructivelyPrependKitten(name){
-   return kittens.unshift('Bob')
- }
- var result = destructivelyPrependKitten(kittens)
- console.log(result);
- 
- function destructivelyRemoveLastKitten(name){
-  return kittens.pop('Garfield')
- }
- var result = destructivelyRemoveLastKitten(kittens)
- console.log(result);
- 
- function destructivelyRemoveFirstKitten(name){
-  return kittens.shift('Milo')
- }
- var result = destructivelyRemoveFirstKitten(kittens)
- console.log(result);
- 
- function appendKitten(name){
-   return [...kittens, 'Broom' ]
+ var kittens = ['Milo', 'Otis', 'Garfield'];
+  
+  function destructivelyAppendKitten(name){
+    return kittens.push(name)
   }
-  var result = appendKitten(kittens)
-  console.log(result);
+  
+  function destructivelyPrependKitten(name){
+    return kittens.unshift(name)
+  }
+  
+  function destructivelyRemoveLastKitten(name){
+    return kittens.pop(name)
+  }
+  
+  function destructivelyRemoveFirstKitten(name){
+    return kittens.shift(name)
+  }
+  
+  function appendKitten(name){
+    return [...kittens, name]
+  }
   
   function prependKitten(name){
-   return ['Arnold', ...kittens]
+    return [name, ...kittens]
   }
-  var result = prependKitten(kittens)
-  console.log(result);
-
-function removeLastKitten(name){
- return kittens.slice(0, kittens.length - 1 )
-}
-var result = removeLastKitten(kittens)
-console.log(result);  
   
-function removeFirstKitten(name){
- return kittens.slice(1) 
-}
-var result = removeFirstKitten(kittens)
-console.log(result);  
+  function removeLastKitten(){
+    return kittens.slice(0, kittens.length-1)
+  }
   
+  function removeFirstKitten(){
+    return kittens.slice(1)
+  }
   
   
   
